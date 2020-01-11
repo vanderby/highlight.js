f8bbfd20249ca1f370c72f0f00b01770d67426ef
Nathan Vanderby <28767221+vanderby@users.noreply.github.com>
2020-01-11 15:32:25 -0600

    Initial commit of KQL to get testing to work.

---
+++ src/languages/kql.js
@@ -0,0 +1,57 @@
+/*
+Language: Kusto Query Language (Azure Log Analytics/Azure Data Explorer)
+Author: Nathan Vanderby <nathan.vanderby@microsoft.com>
+Contributor: 
+Website: https://aka.ms/kql
+Category: common
+*/
+
+function(hljs) {
+  return {
+    aliases: ['kusto', 'kql'],
+    keywords:
+        // Tabular operators 
+        'as consume count datatable distinct evaluate extend externaldata facet find fork getschema invoke join limit lookup' +
+        'make-series mv-apply mv-expand order project project-away project-rename operator project-reorder parse partition print' +
+        'range reduce render sample sample-distinct search serialize sort summarize take top top-nested top-hitters union where',
+    case_insensitive: true,
+    illegal: /[<>{}*]/,
+    contains: [
+      {
+        beginKeywords:
+          'TODO',
+        end: /;/, endsWithParent: true,
+        keywords: {
+          keyword:
+            'TODO',
+          literal:
+            'TODO',
+          built_in:
+            'TODO'
+        },
+        contains: [
+          {
+            className: 'string',
+            begin: '\'', end: '\'',
+            contains: [hljs.BACKSLASH_ESCAPE, {begin: '\'\''}]
+          },
+          {
+            className: 'string',
+            begin: '"', end: '"',
+            contains: [hljs.BACKSLASH_ESCAPE, {begin: '""'}]
+          },
+          {
+            className: 'string',
+            begin: '`', end: '`',
+            contains: [hljs.BACKSLASH_ESCAPE]
+          },
+          hljs.C_NUMBER_MODE,
+          hljs.C_BLOCK_COMMENT_MODE,
+          hljs.HASH_COMMENT_MODE
+        ]
+      },
+      hljs.C_BLOCK_COMMENT_MODE,
+      hljs.HASH_COMMENT_MODE
+    ]
+  };
+}
