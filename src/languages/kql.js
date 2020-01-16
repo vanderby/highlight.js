60c5e00b3ec2bbf5aafbece593fae0456082bf08
Nathan Vanderby <28767221+vanderby@users.noreply.github.com>
2020-01-16 13:03:46 -0600

    Remove incorrect comment modes. Add start attribute.

--- src/languages/kql.js
+++ src/languages/kql.js
@@ -11,38 +11,17 @@ function(hljs) {
     aliases: ['kusto', 'kql'],
     case_insensitive: true,
     illegal: /[<>{}*]/,
-    contains: [
-      {
-        className: 'keyword',
-        begin: '|',
-        keywords:
+    begin: '\|',
+    keywords:
         // Tabular operators 
         'as consume count datatable distinct evaluate extend externaldata facet find fork getschema invoke join limit lookup' +
         'make-series mv-apply mv-expand order project project-away project-rename operator project-reorder parse partition print' +
         'range reduce render sample sample-distinct search serialize sort summarize take top top-nested top-hitters union where',
-        contains: [
-          {
-            className: 'string',
-            begin: '\'', end: '\'',
-            contains: [hljs.BACKSLASH_ESCAPE, {begin: '\'\''}]
-          },
-          {
-            className: 'string',
-            begin: '"', end: '"',
-            contains: [hljs.BACKSLASH_ESCAPE, {begin: '""'}]
-          },
-          {
-            className: 'string',
-            begin: '`', end: '`',
-            contains: [hljs.BACKSLASH_ESCAPE]
-          },
-          hljs.C_NUMBER_MODE,
-          hljs.C_BLOCK_COMMENT_MODE,
-          hljs.HASH_COMMENT_MODE
-        ]
-      },
-      hljs.C_BLOCK_COMMENT_MODE,
-      hljs.HASH_COMMENT_MODE
+    contains: 
+    [
+      hljs.C_LINE_COMMENT_MODE,
+      hljs.APOS_STRING_MODE,
+      hljs.QUOTE_STRING_MODE
     ]
   };
 }
